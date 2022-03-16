a7715979a326e43c7596599d745df8cdaa7276b4
Danva <vuanhdan123@gmail.com>
2022-03-16 15:05:52 +0700

    switch macbook

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,4 @@
 $(document).ready(function () {
-    //Click menu
-    $('.nav-icon').click(function (e) { 
-        e.preventDefault();
-        $('.menu').addClass('active');
-        $('.cover').addClass('active');
-    });
-
-    $('.cover').click(function (e) { 
-        e.preventDefault();
-        $('.menu').removeClass('active');
-        $('.cover').removeClass('active');
-    });
     //slider
-    // $('.strength').slick();
+    $('.strength').slick();
 });
